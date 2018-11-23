72258676f5fa1bc307d07b48bd51d5910c96530c
unic9 <unic9@DESKTOP-FQKFD1H>
2018-11-23 11:50:03 +0900

    첫 업로드 입니다.

---
+++ MyGit/src/com/lec/java/GitClass.java
@@ -0,0 +1,10 @@
+package com.lec.java;
+/*
+ * 이 클래스는 Git 업로드 테스트 용입니다.
+ * @author mainuser
+ */
+public class GitClass {
+	public static void main(String[] args) {
+		System.out.println("GitHub 에 올리기");
+	}
+}
